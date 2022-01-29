16824eee970b6e2fc737c8bb686c4a6042378007
seajaysec <7016191+seajaysec@users.noreply.github.com>
2022-01-29 10:49:45 -0500

    January 29, 2022 10:49 AM

--- asy.py
+++ asy.py
@@ -1,7 +1,13 @@
+y_is_vowel = "UNKNOWN"
 word_up = input("Enter your word: ").upper()
 print(word_up)
 
 if "Y" in word_up:
     print("Y found. Checking vowel status.")
+    if {A, E, I, O, U} in word_up:
+        print("Other vowels identified.")
+    else:
+        print("No other vowels identfied")
+        y_is_vowel = "FALSE"
 else:
     print("No Y found.")
