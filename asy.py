e6d50b40f6ae67ed889aa936be872d5dd1cd3e0b
seajaysec <7016191+seajaysec@users.noreply.github.com>
2022-01-29 13:42:22 -0500

    January 29, 2022 1:42 PM

--- asy.py
+++ asy.py
@@ -6,7 +6,7 @@ Identifies whether Y is a consonant or vowel in a given word.
 y_vowel_state = False
 static_vowels = ["A", "E", "I", "O", "U"]
 word_up = input("Enter your word: ").upper()
-print(word_up)
+print("Entered word is: " + word_up)
 
 if "Y" in word_up:
     print("Y found. Checking vowel status.")
